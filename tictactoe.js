1f50564007101927c62c49760c30d3a64a6f73cf
RaspberryDuck <96967449+RaspberryDuck@users.noreply.github.com>
2023-10-20 22:22:57 -0400

    Create Game() that plays 5 rounds and says winner

--- tictactoe.js
+++ tictactoe.js
@@ -14,16 +14,41 @@ function OneRound(computerChoice, playerChoice){
          (computerChoice == "PAPER" && playerChoiceUpperCase == "ROCK") ||
          (computerChoice == "SCISSORS" && playerChoiceUpperCase == "PAPER")) {
             computerRoundScore = 1;
-            console.log("You lose!" + computerChoice + " beats " + playerChoiceUpperCase);
+            console.log("Computer chose " + computerChoice + " and you chose " + playerChoiceUpperCase +
+            ". You lose! " + computerChoice + " beats " + playerChoiceUpperCase);
     }
     else if (computerChoice === playerChoiceUpperCase) {
         computerRoundScore = 0;
-        console.log("It's a tie!");
+        console.log("Computer chose " + computerChoice + " and you chose " + playerChoiceUpperCase + ". It's a tie!");
     }
     else {
         computerRoundScore = -1;
-        console.log("You win!" + playerChoiceUpperCase + " beats " + computerChoice);
+        console.log("Computer chose " + computerChoice + " and you chose " + playerChoiceUpperCase + ". You win!" + playerChoiceUpperCase + " beats " + computerChoice);
     }
 
     return computerRoundScore;
 }
+
+
+function Game(){
+
+    let playerInput;
+    let totalScore = 0;
+
+    for (let i = 0; i < 5; i ++){
+        playerInput = prompt("Enter either Rock, Paper, or Scissors");
+        totalScore += OneRound(ComputerChoice(), playerInput)
+    }
+
+    if (totalScore > 0){
+        console.log("The winner is the computer. Better luck next time!");
+    }
+    else if (totalScore < 0){
+        console.log("You are the winner, congrats!");
+    }
+    else {
+        console.log("It's a tie, wow!");
+    }
+}
+
+Game();
