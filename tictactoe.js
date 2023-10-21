587d799a1c487ffa8a655e4a210d79ee66af538d
RaspberryDuck <96967449+RaspberryDuck@users.noreply.github.com>
2023-10-20 21:27:25 -0400

    Add function with computer and player inputs

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,37 @@
 function ComputerChoice(){
-    const gameChoices = ["Rock", "Paper", "Scissors"];
+    const gameChoices = ["ROCK", "PAPER", "SCISSORS"];
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice = gameChoices[randomNum];
     return computerChoice;
+}
+
+function OneRound(computerChoice, playerChoice){
+
+    let playerChoiceUpperCase = playerChoice.toUpperCase();
+
+    let computerChoiceValue;
+    let playerChoiceValue;
+
+    if (computerChoice == "ROCK"){
+        computerChoiceValue = 1;
+    }
+    else if (computerChoice == "PAPER"){
+        computerChoiceValue = 2;
+    }
+    else {
+        computerChoiceValue = 3;
+    }
+
+    if (playerChoiceUpperCase == "ROCK") {
+        playerChoiceValue = 1;
+    }
+    else if (playerChoiceUpperCase == "PAPER"){
+        playerChoiceValue = 2;   
+    }
+    else if (playerChoiceUpperCase == "SCISSORS"){
+        playerChoiceValue = 3;
+    }
+    else {
+        console.log("Sorry, invalid value");
+    }
 }
